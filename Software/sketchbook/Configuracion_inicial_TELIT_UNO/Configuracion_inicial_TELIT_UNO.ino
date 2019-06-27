eb40801bdb6ef3600545f68c9d3a5d0cff8cd305
MaycoMD <mayco.md@hotmail.com>
2019-06-27 15:58:19 -0300

    modificaciones menores en el soft

--- Software/sketchbook/Configuracion_inicial_TELIT_UNO/Configuracion_inicial_TELIT_UNO.ino
+++ Software/sketchbook/Configuracion_inicial_TELIT_UNO/Configuracion_inicial_TELIT_UNO.ino
@@ -77,6 +77,7 @@ void loop()
   comandoAT("AT+CSDF=1,2", "OK", 10);
   comandoAT("AT+CSQ", "OK", 10);
   comandoAT("AT+CCLK?", "OK", 10);
+  comandoAT("AT+CCLK=\"2019/06/27,17:52:00+00\"","OK",10);
   //comandoAT("AT+CALA=\"19:30:00+00\",0,4,,\"0\",0", "OK", 10);
 
   // --------- SMS ----------
